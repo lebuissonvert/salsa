6c0b770dc705ad3fbbd140ef5e4a436c4d55614c
Your Name <you@example.com>
2022-07-21 11:48:01 +0200

    Ajout accessibilite romain33ddns

--- src/main/java/app/controller/RestController.java
+++ src/main/java/app/controller/RestController.java
@@ -16,7 +16,7 @@ import java.util.*;
 @org.springframework.web.bind.annotation.RestController
 @CrossOrigin(
         //origins = "http://localhost:4200",
-        origins = "http://192.168.1.100",
+        origins = {"http://192.168.1.100", "http://passes-salsa.ddns.net", "http://romain33.ddns.net"},
         //origins = "http://passes-salsa.ddns.net",
         allowedHeaders = "*",
         allowCredentials = "true"
